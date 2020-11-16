98478b74bdc5deb70cbd50577e708bc09af68d24
Leeonardoo <leeonardo.deo@gmail.com>
2020-11-16 20:45:02 -0300

    Adicionado enigma dos números em enigmas_previos.docx Adicionado uma tabela no layout de Enigma2Logica

--- src/Enigma2Logica.java
+++ src/Enigma2Logica.java
@@ -6,11 +6,14 @@ import java.awt.Font;
 import javax.swing.JRadioButton;
 import javax.swing.JButton;
 import javax.swing.ButtonGroup;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 public class Enigma2Logica {
 
 	private JFrame frame;
 	private final ButtonGroup buttonGroup = new ButtonGroup();
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -43,38 +46,49 @@ public class Enigma2Logica {
 		frame.setBounds(100, 100, 350, 250);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
-		JLabel lblNewLabel = new JLabel("<html>\r\n__________________ <br>\r\n| 288 | 576 |   72  | <br>\r\n| 144 | 360 |   ?    | <br>\r\n| 504 | 648 | 216  | <br>\r\n|________________ | <br>\r\n</html>");
-		lblNewLabel.setBounds(10, 39, 121, 81);
-		frame.getContentPane().add(lblNewLabel);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("<html>Assinale a alternativa que corresponde ao n\u00FAmero faltando da tabela abaixo");
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		lblNewLabel_1.setBounds(10, 11, 314, 29);
 		frame.getContentPane().add(lblNewLabel_1);
-		
+
 		JRadioButton rdbtnNewRadioButton = new JRadioButton("A - 327");
 		buttonGroup.add(rdbtnNewRadioButton);
 		rdbtnNewRadioButton.setBounds(10, 131, 73, 23);
 		frame.getContentPane().add(rdbtnNewRadioButton);
-		
+
 		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("B - 485");
 		buttonGroup.add(rdbtnNewRadioButton_1);
 		rdbtnNewRadioButton_1.setBounds(10, 169, 73, 23);
 		frame.getContentPane().add(rdbtnNewRadioButton_1);
-		
+
 		JRadioButton rdbtnNewRadioButton_2 = new JRadioButton("C - 785");
 		buttonGroup.add(rdbtnNewRadioButton_2);
 		rdbtnNewRadioButton_2.setBounds(85, 131, 73, 23);
 		frame.getContentPane().add(rdbtnNewRadioButton_2);
-		
+
 		JRadioButton rdbtnNewRadioButton_3 = new JRadioButton("D - 432");
 		buttonGroup.add(rdbtnNewRadioButton_3);
 		rdbtnNewRadioButton_3.setBounds(85, 169, 73, 23);
 		frame.getContentPane().add(rdbtnNewRadioButton_3);
-		
+
 		JButton btnNewButton = new JButton("Confirmar");
 		btnNewButton.setBounds(210, 148, 89, 23);
 		frame.getContentPane().add(btnNewButton);
+
+		table = new JTable();
+		table.setEnabled(false);
+		table.setModel(new DefaultTableModel(
+				new Object[][] {
+						{"288", "576", "72"},
+						{"144", "360", "??"},
+						{"504", "648", "216"},
+				},
+				new String[] {
+						"New column", "New column", "New column"
+				}
+		));
+		table.setBounds(53, 61, 225, 48);
+		frame.getContentPane().add(table);
 	}
 }
